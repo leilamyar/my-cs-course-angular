9c3046ca1ccdfc73868405e2e1b7087868bee66d
leilamyar <leilamyar.dev@gmail.com>
2021-10-26 14:20:45 +0200

    init commit creat ng app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Host, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataStructuresComponent } from './data-structures/data-structures.component';
+import { HomeComponent } from './home/home.component';
+import { SortAlgoComponent } from './sort-algo/sort-algo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'data-structures', component: DataStructuresComponent },
+  { path: 'sort-algo', component: SortAlgoComponent },
+  // { path: 'sort-algo', component: SortAlgoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
